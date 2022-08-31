af63ebd21c7ff37341e868c15e67b9f84a3c8bb5
jinwei69 <ss610349@gmail.com>
2022-08-31 09:59:20 +0800

    add bat腳本

---
+++ clones_others_project.bat
@@ -0,0 +1,5 @@
+cd ../
+git clone http://gitlab.gpmcorp.com.tw/lat/ganghaoagv.git
+git clone http://gitlab.gpmcorp.com.tw/lat/gpm_agv_lat_web.git
+git clone http://gitlab.gpmcorp.com.tw/lat/gpm_agv_lat_app.git
+pause
---
+++ pull_all_projects.bat
@@ -0,0 +1,10 @@
+cd ../
+cd ganghaoagv
+git clone http://gitlab.gpmcorp.com.tw/lat/ganghaoagv.git
+cd ../
+cd gpm_agv_lat_web
+git clone http://gitlab.gpmcorp.com.tw/lat/gpm_agv_lat_web.git
+cd ../
+cd gpm_agv_lat_app
+git clone http://gitlab.gpmcorp.com.tw/lat/gpm_agv_lat_app.git
+pause
